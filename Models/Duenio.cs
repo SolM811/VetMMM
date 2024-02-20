5add8fec5b4b7ead935259809758f5feb8103e6c
Sol Muñiz <Sol Muiz@DESKTOP-849PME1>
2024-02-20 15:31:23 -0600

    Agregar archivos de proyecto.

---
+++ Models/Duenio.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace VetMMM.Models
+{
+    [Table("Duenios")]
+    public class Duenio
+    {
+       
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string nombreD { get; set; }
+        public int telefonoD { get; set; }
+        public string domicilioD { get; set; }
+    }
+}
---
+++ Models/Mascota.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace VetMMM.Models
+{
+    [Table("Mascotas")]
+    public class Mascota
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string nombreM { get; set; }
+        public string especieM { get; set; }
+        public string fotoM { get; set; }
+        public string razaM { get; set; }
+        public int edadM { get; set; }
+        public virtual Duenio duenio { get; set; }
+    }
+}
